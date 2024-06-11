a0ac882baf3579a599c01d166931de3859459d08
jiieunshin <jieunstat@gmail.com>
2024-06-11 15:37:42 +0900

    24.06.11

--- R/coxfuncs.R
+++ R/coxfuncs.R
@@ -184,22 +184,22 @@ cv.gettheta = function (model, x, time, status, mscale, lambda0, lambda_theta, g
     if(algo == "CD"){
       init.theta = rep(1, d)
 
-      # Gw = G * sqrt(model$w.new)
-      # uw = model$zw.new - model$b.new * sqrt(model$w.new) - (n/2) * lambda0 * model$cw.new
-      # theta.new = .Call("theta_step", Gw, uw, n, d, init.theta, lambda_theta[k], gamma)
-      # save_theta[[k]] <- theta.new
-      #
-      # XX = model$zw.new - Gw %*% theta.new
-      # num = t(XX) %*% XX + 1
-      # den = (1 - sum(diag( Gw %*% ginv( t(Gw) %*% Gw) %*% t(Gw) )) / n)^2 + 1
+      Gw = G * sqrt(model$w.new)
+      uw = model$zw.new - model$b.new * sqrt(model$w.new) - (n/2) * lambda0 * model$cw.new
+      theta.new = .Call("theta_step", Gw, uw, n, d, init.theta, lambda_theta[k], gamma)
+      save_theta[[k]] <- theta.new
 
-
-      fit = gettheta.cd(init.theta, model$f.new, G, time, status, model$b.new, (n/2) * lambda0 * model$cw.new, lambda_theta[k], gamma, RS)
-      save_theta[[k]] <- fit$theta.new
-
-      XX = fit$zw.new - fit$Gw %*% fit$theta.new
+      XX = model$zw.new - Gw %*% theta.new
       num = t(XX) %*% XX + 1
-      den = (1 - sum(diag( fit$Gw %*% ginv( t(fit$Gw) %*% fit$Gw) %*% t(fit$Gw) )) / n)^2 + 1
+      den = (1 - sum(diag( Gw %*% ginv( t(Gw) %*% Gw) %*% t(Gw) )) / n)^2 + 1
+
+
+      # fit = gettheta.cd(init.theta, model$f.new, G, time, status, model$b.new, (n/2) * lambda0 * model$cw.new, lambda_theta[k], gamma, RS)
+      # save_theta[[k]] <- fit$theta.new
+      #
+      # XX = fit$zw.new - fit$Gw %*% fit$theta.new
+      # num = t(XX) %*% XX + 1
+      # den = (1 - sum(diag( fit$Gw %*% ginv( t(fit$Gw) %*% fit$Gw) %*% t(fit$Gw) )) / n)^2 + 1
 
       measure[k] <- as.vector(num / den / n)
 
