9ca16fad8d4a33b94dbd84683d0ac7ee0248ac30
jiieunshin <jieunstat@gmail.com>
2024-08-27 17:54:55 +0900

    24.08.27

--- R/data_generation.R
+++ R/data_generation.R
@@ -28,6 +28,7 @@ data_generation = function(n, p, rho, SNR,
     f2 = function(t) (2 * t - 1)^2
     f3 = function(t) sin(2 * pi * t) / (2 - sin(2 * pi * t))
     f4 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3
+    f5 = function(t) sin(pi * t^4) + t^4
   }
 
   # f1 = function(t) 5 * sin(3*t)
@@ -70,9 +71,9 @@ data_generation = function(n, p, rho, SNR,
 
 
 
-  f = 5 * f1(x[,1]) + 3 * f2(x[,2]) + 4 * f3(x[,3]) + 6 * f4(x[,4]) + 4 * f5(x[,5])
+  f = 5 * f1(x[,1]) + 2 * f2(x[,2]) + 3 * f3(x[,3]) + 6 * f4(x[,4]) + 4 * f5(x[,5])
   # print(f)
-  V_sig = var(5 * f1(x[,1])) + var(3 * f2(x[,2])) + var(4 * f3(x[,3])) + var(6 * f4(x[,4])) + var(4 * f5(x[,5]))
+  V_sig = var(5 * f1(x[,1])) + var(2 * f2(x[,2])) + var(3 * f3(x[,3])) + var(6 * f4(x[,4])) + var(4 * f5(x[,5]))
   sd = sqrt(V_sig / SNR)
 
 
@@ -82,7 +83,7 @@ data_generation = function(n, p, rho, SNR,
   # sd = .1
 print(sd)
 
-  x_nois = pnorm(matrix(rnorm(n * (p-5), 0, 1), n, (p-5)))
+  x_nois = pnorm(matrix(rnorm(n * (p-5), 0, sd/sqrt(p-5)), n, (p-5)))
   # x_nois = matrix(runif(n * (p-4)), n, (p-4))
   x = cbind(x, x_nois)
 
@@ -103,7 +104,7 @@ print(sd)
   if(response == "regression"){
     # SNR = sqrt(.6*(p-5)) # SNR = 4일 때
     # print(SNR)
-    f = f + rnorm(n, 0, sd)
+    # f = f + rnorm(n, 0, sd)
 
     out = list(x = x, y = f)
   }
