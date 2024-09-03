1c7bc3a0e0847b8e5a7ad592c979ac509f4787e3
jiieunshin <jieunstat@gmail.com>
2024-09-03 18:44:10 +0900

    24.09.03

--- R/data_generation.R
+++ R/data_generation.R
@@ -18,9 +18,10 @@ data_generation = function(n, p, rho, SNR,
   # if(response == "classification"){
     # f1 = function(t) t - 0.5
     # f2 = function(t) (2 * t - 1)^2 - 0.4
-    # f3 = function(t) sin(2 * pi * t) / (2 - sin(2 * pi * t)) - 0.1
-    # f4 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3 - 0.4 - 0.4
-  #   f5 = function(t) sin(pi * t^4) + t^4 - 0.5
+    # f3 = function(t) sin(2 * pi * t) / (2 - sin(2 * pi * t))
+    # f4 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3 - 0.2
+    # f5 = function(t) sin(pi * t^4) + t^4 - 0.6
+  #
   # }
   #
   # if(response == "regression"){
@@ -28,7 +29,7 @@ data_generation = function(n, p, rho, SNR,
     f2 = function(t) (2 * t - 1)^2
     f3 = function(t) sin(2 * pi * t) / (2 - sin(2 * pi * t))
     f4 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3
-    # f5 = function(t) sin(pi * t^4) + t^4
+    f5 = function(t) sin(pi * t^4) + t^4
   # }
 
   # f1 = function(t) 5 * sin(3*t)
@@ -43,17 +44,17 @@ data_generation = function(n, p, rho, SNR,
 
   if(missing(n)) n = 200
   if(missing(p)) p = 10
-  if(missing(rho)) rho = 0.5
+  if(missing(rho)) rho = 0.8
   if(missing(SNR)) SNR = 8
 
   if(p <= 5) stop("dimension size should be larger than 5.")
 
-  Sigma = matrix(rho, 4, 4)
+  Sigma = matrix(rho, 5, 5)
   diag(Sigma) = 1
 
-  # Sigma = matrix(1, 4, 4)
-  # for(j in 1:4){
-  #   for(k in 1:4){
+  # Sigma = matrix(1, 5, 5)
+  # for(j in 1:5){
+  #   for(k in 1:5){
   #     Sigma[j, k] = rho^abs(j-k)
   #   }
   # }
@@ -71,10 +72,10 @@ data_generation = function(n, p, rho, SNR,
 
 
 
-  f = 5 * f1(x[,1]) + 3 * f2(x[,2]) + 4 * f3(x[,3]) + 6 * f4(x[,4])
+  f = 5 * f1(x[,1]) + 3 * f2(x[,2]) + 4 * f3(x[,3]) + 6 * f4(x[,4]) + 2 * f5(x[,5])
   # + 3 * f5(x[,5])
   # print(f)
-  V_sig = var(5 * f1(x[,1])) + var(3 * f2(x[,2])) + var(4 * f3(x[,3])) + var(6 * f4(x[,4]))
+  V_sig = var(5 * f1(x[,1])) + var(3 * f2(x[,2])) + var(4 * f3(x[,3])) + var(6 * f4(x[,4])) + var(2 * f5(x[,5]))
   sd = sqrt(V_sig / SNR)
 
 
@@ -84,7 +85,7 @@ data_generation = function(n, p, rho, SNR,
   # sd = .1
 print(sd)
 
-  x_nois = pnorm(matrix(rnorm(n * (p-4), 0, sd/sqrt(p-4)), n, (p-4)))
+  x_nois = pnorm(matrix(rnorm(n * (p-5), 0, sd/sqrt(p-5)), n, (p-5)))
   # x_nois = matrix(runif(n * (p-4)), n, (p-4))
   x = cbind(x, x_nois)
 
@@ -121,12 +122,7 @@ print(sd)
   }
 
   if(response == "count"){
-    mu = exp(f/sqrt(2*p))
-
-    # SNR = sqrt(var(f) / 4)
-    # SNR = sqrt(.6*(p-5))
-    # e = rnorm(n, 0, SNR)
-    # mu = exp(f)
+    mu = exp(f)
     y = rpois(n, mu)
     out = list(x = x, f = f, y = y)
   }
@@ -176,29 +172,4 @@ print(sd)
   }
   return(out)
 }
-
-
-# tr = data_generation(100, 50, response = "regression")
-# tr_x = tr$x
-# tr_y = tr$y
-# try(cdcosso(tr_x, tr_y, family = 'Cox', gamma = 0.95, kernel = "spline", scale = T, algo = "CD",
-#             lambda_theta = exp(seq(log(2^{-35}), log(2^{2}), length.out = 20))
-# ), silent = TRUE)
-
-# fit10 = try(cdcosso(tr_x, tr_y, family = 'Cox', gamma = 0.95, kernel = "spline", scale = T, algo = "CD"), silent = TRUE)
-# print(fit10$theta_step$theta.new)
-# par(mfrow = c(1, 5))
-# for(i in 1:5)
-#   plot(c(te_x[, i]), en1_pred$f.new, main = i)
-
-
-# ff = function(t) 3*((3 * t - 2)^2 - 1)
-# plot(te_x[,5], ff(te_x[,5]), cex = .6, pch = 16, xlab = 'x5', ylab = 'f5')
-# fff = ff(te_x[,5])
-# prob = exp(fff)/(exp(fff) + 1)
-# y = rbinom(n, 1, prob)
-# plot(prob)
-# print(table(y))
-# plot(tr$f)
-
 
