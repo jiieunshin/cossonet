c69bcf773d6f1b3d460ae8c76fcd4c6fe0abbb29
jiieunshin <jieunstat@gmail.com>
2024-10-23 11:57:56 +0900

    24.10.23

--- R/data_generation.R
+++ R/data_generation.R
@@ -34,16 +34,16 @@ data_generation = function(n, p, rho, SNR,
   }
 
   if(response == "count"){
-  f1 = function(t) t
-  f2 = function(t) (2 * t - 1)^2
-  f3 = function(t) sin(2 * pi * t) + 1
-  f4 = function(t) exp(t) - 1
+  f1 = function(t) - t
+  f2 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3
+  f3 = function(t) - sin(2 * pi * t)
+  f4 = function(t) t^2
   }
 
   if(response == "survival"){
     f1 = function(t) t
     f2 = function(t) (2 * t - 1)^2
-    f3 = function(t) sin(2 * pi * t) / (2 - sin(2 * pi * t))
+    f3 = function(t) - 2 * sin(2 * pi * t) / (2 - sin(2 * pi * t))
     f4 = function(t) 0.1*sin(2 * pi * t) + 0.2*cos(2 * pi * t) + 0.3*sin(2 * pi * t)^2 + 0.4*cos(2 * pi * t)^3 + 0.5*sin(2 * pi * t)^3
   }
 
@@ -127,7 +127,7 @@ data_generation = function(n, p, rho, SNR,
   }
 
   if(response == "count"){
-    f = - f1(x[,1]) + f2(x[,2]) + f3(x[,3]) + f4(x[,4])
+    f = f1(x[,1]) + 2 * f2(x[,2]) + 2 * f3(x[,3]) +  2 * f4(x[,4])
     V_sig = var(f1(x[,1])) + var(f2(x[,2])) + var(f3(x[,3])) + var(f4(x[,4]))
 
     # f = 1 * f1(x[,1]) + 2 * f2(x[,2]) + 3 * f3(x[,3]) + 6 * f4(x[,4])
