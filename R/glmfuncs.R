5eb729d92d1b0f68bb2149ec77695ce7cbffadc6
jiieunshin <jieunstat@gmail.com>
2024-11-15 12:37:54 +0900

    24.11.15

--- R/glmfuncs.R
+++ R/glmfuncs.R
@@ -129,10 +129,10 @@ cv.sspline.subset = function (K, y, f, nbasis, basis.id, mscale, cand.lambda, ob
       testfhat = c(b.new + te_Rtheta %*% c.new)
       mu = obj$linkinv(f)
 
-      # if(obj$family == "gaussian") measure[f, k] <- mean((testfhat - y[te_id])^2)
-      # if(obj$family == "binomial") measure[f, k] <- mean(y[te_id] != ifelse(testmu < 0.5, 0, 1))
-      # if(obj$family == "poisson") measure[f, k] <- mean(poisson()$dev.resids(y[te_id], testmu, rep(1, te_n)))
-      measure[fid, k] <- KL(testfhat, mu, obj)
+      if(obj$family == "gaussian") measure[f, k] <- mean((testfhat - y[te_id])^2)
+      if(obj$family == "binomial") measure[f, k] <- mean(y[te_id] != ifelse(testmu < 0.5, 0, 1))
+      if(obj$family == "poisson") measure[f, k] <- mean(poisson()$dev.resids(y[te_id], testmu, rep(1, te_n)))
+      # measure[fid, k] <- KL(testfhat, mu, obj)
       # measure[f, k] <- SKL(te_Rtheta %*% c.new, testfhat)
 
     }
@@ -357,10 +357,10 @@ cv.nng.subset = function(model, K, y, f, nbasis, basis.id, mscale, lambda0, lamb
 
       # testfhat = G[te_id, ] %*% theta.adj
       mu = obj$linkinv(f)
-      # if(obj$family == "gaussian") measure[f, k] <- mean((testfhat - y[te_id])^2)
-      # if(obj$family == "binomial") measure[f, k] <- mean(y[te_id] != ifelse(testmu < 0.5, 0, 1))
-      # if(obj$family == "poisson") measure[f, k] <- mean(poisson()$dev.resids(y[te_id], testmu, rep(1, te_n)))
-      measure[fid, k] <- KL(testfhat, mu, obj)
+      if(obj$family == "gaussian") measure[f, k] <- mean((testfhat - y[te_id])^2)
+      if(obj$family == "binomial") measure[f, k] <- mean(y[te_id] != ifelse(testmu < 0.5, 0, 1))
+      if(obj$family == "poisson") measure[f, k] <- mean(poisson()$dev.resids(y[te_id], testmu, rep(1, te_n)))
+      # measure[fid, k] <- KL(testfhat, mu, obj)
     }
   }
 print(measure)
