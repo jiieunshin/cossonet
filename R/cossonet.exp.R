fdde4cdfafbdb08f42e51fd3bedbced5a2afd905
jiieunshin <jieunstat@gmail.com>
2025-03-18 15:16:33 +0900

    24.03.18

--- R/cossonet.exp.R
+++ R/cossonet.exp.R
@@ -3,7 +3,7 @@ cossonet.exp = function (x, y, wt, nbasis, basis.id, lambda0, lambda_theta, gamm
   n = length(y)
   p = length(wt)
 
-  message("fit COSSO  with n = ", n, "p =", ncol(x), "\n")
+  message("fit COSSO  with n = ", n, ", p =", ncol(x), "\n")
 
   if (missing(nbasis) & missing(basis.id)) {
     nbasis = max(40, ceiling(12 * n^(2/9)))
@@ -18,21 +18,23 @@ cossonet.exp = function (x, y, wt, nbasis, basis.id, lambda0, lambda_theta, gamm
 
   K = make_anovaKernel(x, x, type = type, kparam, scale)
   d = K$numK
-  message("kernel:", type, "and d =", d, "\n")
+  mscale = rep(1, d)
+
+  message("kernel: ", type, " and d =", d, "\n")
 
   op <- par(no.readonly = TRUE)
   on.exit(par(op))
 
   par(mfrow = c(1,2))
   # solve (theta) - 1st
-  sspline_cvfit = cv.sspline.subset(K, y, nbasis, basis.id, rep(1, p)/wt^2, lambda0, obj, type, nfold, kparam, one.std = one.std, show = TRUE)
+  sspline_cvfit = cv.sspline.subset(K, y, nbasis, basis.id, rep(1, d)/mscale^2, lambda0, obj, type, nfold, kparam, one.std = one.std, show = TRUE)
 
   # solve (b, c) - 1st
-  nng_fit = cv.nng.subset(sspline_cvfit, K, y, nbasis, basis.id, wt, sspline_cvfit$optlambda, lambda_theta, gamma, nfold, one.std = one.std, obj)
+  nng_fit = cv.nng.subset(sspline_cvfit, K, y, nbasis, basis.id, mscale, sspline_cvfit$optlambda, lambda_theta, gamma, nfold, one.std = one.std, obj)
   theta.new = rescale_theta(nng_fit$theta.new)
 
   # solve (theta) - 2nd
-  sspline_cvfit = cv.sspline.subset(K, y, nbasis, basis.id, rep(1, p) / wt^2, lambda0, obj, type, nfold, kparam, one.std = FALSE, show = FALSE)
+  sspline_cvfit = cv.sspline.subset(K, y, nbasis, basis.id, theta.new/mscale^2, lambda0, obj, type, nfold, kparam, one.std = FALSE, show = FALSE)
 
   out = list(data = list(x = x, y = y, basis.id = basis.id, wt = wt, kernel = type, kparam = kparam),
              tune = list(lambda0 = lambda0, lambda_theta = lambda_theta, gamma = gamma),
