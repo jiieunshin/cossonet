f5c67e7949f1482b5d52dc4bec37d756ea51ead5
jieun shin <jieunstat@gmail.com>
2026-01-08 10:46:01 +0900

    26.01.08

--- R/cossonet.exp.R
+++ R/cossonet.exp.R
@@ -134,23 +134,23 @@ cossonet.exp = function (x, y, wt, nbasis, basis.id,
   ## --------------------------
   ## (F) second theta refinement
   ## --------------------------
-  # sspline_cvfit2 = cv.sspline.subset(
-  #   K        = K,
-  #   y        = y,
-  #   nbasis   = nbasis,
-  #   basis.id = basis.id,
-  #   mscale   = nng_fit$theta.new/mscale^2,
-  #   c.init   = sspline_cvfit$c.new,
-  #   cand.lambda = sspline_cvfit$opt_lambda0,
-  #   obj      = obj,
-  #   type     = type,
-  #   cv       = "GCV",
-  #   nfold    = 1,
-  #   one.std  = FALSE,
-  #   show     = FALSE
-  # )
-  
-  # rm(sspline_cvfit)
+  # # sspline_cvfit2 = cv.sspline.subset(
+  # #   K        = K,
+  # #   y        = y,
+  # #   nbasis   = nbasis,
+  # #   basis.id = basis.id,
+  # #   mscale   = nng_fit$theta.new/mscale^2,
+  # #   c.init   = sspline_cvfit$c.new,
+  # #   cand.lambda = sspline_cvfit$opt_lambda0,
+  # #   obj      = obj,
+  # #   type     = type,
+  # #   cv       = "GCV",
+  # #   nfold    = 1,
+  # #   one.std  = FALSE,
+  # #   show     = FALSE
+  # # )
+  # 
+  # # rm(sspline_cvfit)
   # par(mfrow = c(1,1))
   
   ## --------------------------
@@ -167,7 +167,7 @@ cossonet.exp = function (x, y, wt, nbasis, basis.id,
       kparam    = kparam
     ),
     tune = list(lambda0 = lambda0, lambda_theta = lambda_theta, gamma = gamma),
-    c_step = sspline_cvfit,
+    c_step = sspline_cvfit2,
     theta_step = nng_fit,
     family = obj$family
   )
